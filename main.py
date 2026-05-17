e71b9e164e698cde11e8a625c8a265acb4f1b80f
abhishek <abhi86.pathak@gmail.com>
2026-05-17 14:03:16 +0530

    First api

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("heloo")
+from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return JSONResponse(status_code=401, content="Hello")
+
+    
